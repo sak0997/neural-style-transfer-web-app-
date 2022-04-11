efb3b68bcd71e63d4fcd2a1bb4c05754b4a04eb3
sak0997 <103422707+sak0997@users.noreply.github.com>
2022-04-11 22:53:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.markdown(
 
 
 # создание боковой панели для выбора стиля изображения
-st.image(image=""./images/speed-brush.gif")
+st.image(image="./images/speed-brush.gif")
 st.markdown("</br>", unsafe_allow_html=True)
 style_name = st.sidebar.selectbox(
     'Выберите стиль:',
